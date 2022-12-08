070bc191d0c3ae1ce524ae4d9f80ed518b90f86f
simonemontella <simonemontella8@gmail.com>
2022-12-09 00:51:53 +0100

    Revert "day7, day 8 solved"
    This reverts commit 59b9c975d629dd08dcee7652749ed5010a5b9062.

--- solutions/__init__.py
+++ solutions/__init__.py
@@ -4,8 +4,8 @@ from solutions.day3 import Day3
 from solutions.day4 import Day4
 from solutions.day5 import Day5
 from solutions.day6 import Day6
-from solutions.day7 import Day7
-from solutions.day8 import Day8
+#from solutions.day7 import Day7
+#from solutions.day8 import Day8
 #from solutions.day9 import Day9
 #from solutions.day10 import Day10
 
